cce2a19566bd624c868ec90e22d57d1b0944e878
JoaoNunoAbreu <joaonunocgabreu@gmail.com>
2023-04-18 16:10:44 +0100

    update

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import os
 from sys import argv
 from datetime import datetime
 
+DEFAULT_SECTION = "GENERAL"
+
 dir_path = os.path.dirname(os.path.realpath(__file__))
 file_path = dir_path + "/data.json"
 
@@ -37,15 +39,16 @@ def normalize(info):
             count += 1
     return info
 
+
 def are_there_tasks(data):
     for i in data:
         if data[i] != []:
             return True
     return False
 
-def prettyprint(data):
 
-    if(are_there_tasks(data) == False):
+def prettyprint(data):
+    if (are_there_tasks(data) is False):
         print("No data. Try adding some tasks! Use \"todo help\" for more info.")
         return
 
@@ -73,101 +76,100 @@ def prettyprint(data):
             last_section = i[0]
         else:
             print(f" {' ':<{section_width}} | {i[1]:<{task_width}} | {i[2]:<7}")
-
     print()
 
 
 def main():
-    if((len(sys.argv) == 3 or len(sys.argv) == 4 or len(sys.argv) == 5) and sys.argv[1] == "add"):
+    if ((len(sys.argv) in [3,4,5]) and sys.argv[1] == "add"):
 
         info = readTasks()
 
         # Cria a seccção, caso não exista
-        if(len(sys.argv) == 3 or (len(sys.argv) == 4 and "/" in sys.argv[3])):
-            if("GENERAL" not in info):
-                info["GENERAL"] = []
+        if (len(sys.argv) == 3 or (len(sys.argv) == 4 and "/" in sys.argv[3])):
+            if (DEFAULT_SECTION not in info):
+                info[DEFAULT_SECTION] = []
         else:
-            if(argv[2] not in info and "/" not in sys.argv[3]):
+            if (argv[2] not in info and "/" not in sys.argv[3]):
                 info[sys.argv[2]] = []
 
         # Adiciona nova task ao ficheiro
-        if(len(sys.argv) == 3):
-            newTask(info, "GENERAL", sys.argv[2], "-----")
+        if (len(sys.argv) == 3):
+            newTask(info, DEFAULT_SECTION, sys.argv[2], "-----")
 
-        elif(len(sys.argv) == 4):
+        elif (len(sys.argv) == 4):
             # Caso tenha data sem seção
-            if("/" in sys.argv[3]):
-                newTask(info, "GENERAL", sys.argv[2], sys.argv[3])
+            if ("/" in sys.argv[3]):
+                newTask(info, DEFAULT_SECTION, sys.argv[2], sys.argv[3])
             # Caso tenha secção sem data
             else:
                 newTask(info, sys.argv[2], sys.argv[3], "-----")
 
-        elif(len(sys.argv) == 5):
-            if("/" not in sys.argv[4]):
+        elif (len(sys.argv) == 5):
+            if ("/" not in sys.argv[4]):
                 print("Invalid date...")
                 sys.exit()
             newTask(info, sys.argv[2], sys.argv[3], sys.argv[4])
 
         prettyprint(readTasks())
 
-    elif(len(sys.argv) == 1):
+    elif (len(sys.argv) == 1):
         prettyprint(readTasks())
-    elif(len(sys.argv) == 3 and sys.argv[1] == "rs"):
+    elif (len(sys.argv) == 3 and sys.argv[1] == "rs"):
         info = readTasks()
         seccao = sys.argv[2]
-        if(seccao not in info):
+        if (seccao not in info):
             print("Section doesn't exist...")
             sys.exit(0)
         del info[seccao]
         writeTasks(info)
-    elif(len(sys.argv) == 4 and sys.argv[1] == "rm"):
+    elif (len(sys.argv) == 4 and sys.argv[1] == "rm"):
         info = readTasks()
         seccao = sys.argv[2]
-        if(seccao not in info):
+        if (seccao not in info):
             print("Section doesn't exist...")
             sys.exit(0)
-        if(int(sys.argv[3]) > len(info[seccao]) or int(sys.argv[3]) <= 0):
+        if (int(sys.argv[3]) > len(info[seccao]) or int(sys.argv[3]) <= 0):
             print("Invalid id...")
             sys.exit(0)
 
         found = False
         for i in range(len(info[seccao])):
-            if(info[seccao][i]['id'] == int(sys.argv[3])):
+            if (info[seccao][i]['id'] == int(sys.argv[3])):
                 del info[seccao][i]
                 found = True
                 break
 
-        if(found == False):
+        if (found is False):
             print("Invalid id...")
         else:
             info = normalize(info)
             writeTasks(info)
-    elif(len(sys.argv) == 2 and sys.argv[1] == "dates"):
+    elif (len(sys.argv) == 2 and sys.argv[1] == "dates"):
         info = readTasks()
         info_with_dates = {}
 
         for sec in info:
             for i in info[sec]:
-                if(i["date"] != "-----"):
-                    if(sec not in info_with_dates):
+                if (i["date"] != "-----"):
+                    if (sec not in info_with_dates):
                         info_with_dates[sec] = []
                     info_with_dates[sec].append(i)
         prettyprint(info_with_dates)
 
-    elif(len(sys.argv) == 2 and sys.argv[1] == "today"):
+    elif (len(sys.argv) == 2 and sys.argv[1] == "today"):
         info = readTasks()
         info_with_dates = {}
         todays_date = datetime.now().strftime("%d/%m")
         for sec in info:
             for i in info[sec]:
-                if(i["date"] == todays_date):
-                    if(sec not in info_with_dates):
+                if (i["date"] == todays_date):
+                    if (sec not in info_with_dates):
                         info_with_dates[sec] = []
                     info_with_dates[sec].append(i)
         prettyprint(info_with_dates)
-    elif(len(sys.argv) == 2 and sys.argv[1] == "help"):
-        print("$ todo                                       -> Show the tasks for each section")
-        print("$ todo add \"task\" [\"date\"]                   -> New task to the \"GENERAL\" section")
+    elif (len(sys.argv) == 2 and sys.argv[1] == "help"):
+        print("$ todo" + " "*39 + "-> Show the tasks for each section")
+        print("$ todo add \"task\" [\"date\"]" + " "*19 + f"-> New task to the \"{DEFAULT_SECTION}\" section")
         print("$ todo add \"section_name\" \"task\" [\"date\"]    -> New task to a section")
         print("$ todo rm \"section_name\" \"id-task\"           -> Removes task from a section")
         print("$ todo rs \"section_name\"                     -> Removes a section")
